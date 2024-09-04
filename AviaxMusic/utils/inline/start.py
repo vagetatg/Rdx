c330d490fe91feac4e4c7c8e5e62e1eb78f8779d
Goku malik <168086405+Gokukhan1@users.noreply.github.com>
2024-09-04 23:02:07 +0530

    Update start.py

--- AviaxMusic/utils/inline/start.py
+++ AviaxMusic/utils/inline/start.py
@@ -24,7 +24,7 @@ def private_panel(_):
                 url=f"https://t.me/{app.username}?startgroup=true",
             )
         ],
-        [InlineKeyboardButton(text=_["S_B_4"], callback_data="https://t.me/{app.username}?start=help")],
+        [InlineKeyboardButton(text=_["S_B_4"], url="https://t.me/{app.username}?start=help")],
         [
             InlineKeyboardButton(text=_["S_B_5"], user_id=config.OWNER_ID),
             InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_GROUP),
